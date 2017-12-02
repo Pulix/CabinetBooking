793a292e2e5ef6009320122cafb3156ffa5c124e
Ovreiu Victor <viki_number_1@yahoo.com>
2017-12-02 15:57:48 +0200

    mas

--- CabinetBooking/Register.aspx.cs
+++ CabinetBooking/Register.aspx.cs
@@ -25,7 +25,7 @@ namespace CabinetBooking
 
 			if (password == "")
 			{
-				password = "Parola nana!";
+				password = "Parola changeeeee!";
 			}
 
 			Label1.Text = firstName + " " + lastName + " " + username + " " + password;
