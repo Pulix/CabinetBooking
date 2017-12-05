6c97b5f4340c71b59df4d083a3223fcf8ba1906d
Andrei Nicolae <pulix92@yahoo.com>
2017-12-06 00:44:08 +0200

    Added getUserGender() and getDateOfBirth() methods.

--- CabinetBooking/Helpers/CNPvalidator.cs
+++ CabinetBooking/Helpers/CNPvalidator.cs
@@ -13,10 +13,11 @@ namespace CabinetBooking.Helpers
 		/// </summary>
 		/// <param name="cnp"></param>
 		/// <returns></returns>
-		public bool ValidateCNP(string cnp)
+		public static bool ValidateCNP(string cnp)
 		{
 			string controlNumber = "279146358279";
 			int sum = 0;
+			
 
 			char[] cnpNumbers = cnp.ToCharArray();
 			char[] controlNumbers = controlNumber.ToCharArray();
@@ -26,12 +27,48 @@ namespace CabinetBooking.Helpers
 				sum += (Int32.Parse(cnpNumbers[i].ToString()) * Int32.Parse(controlNumbers[i].ToString()));
 			}
 
-			if ((sum % 11) == Int32.Parse(cnpNumbers[12].ToString()))
+			//If sum % 11 is 10, then the validation number should be 1
+			int validControlNumber = (sum % 11 == 10) ? 1 : sum % 11;
+		
+
+			if (cnp[12] ==  validControlNumber)
 			{
 				return true;
 			}
 
 			return false;
+		}
+
+		/// <summary>
+		/// Get the User's gender from CNP
+		/// </summary>
+		/// <param name="cnp"></param>
+		/// <returns></returns>
+		public static string getUserGender(string cnp)
+		{
+			if (cnp[0].ToString() == "1" || cnp[0].ToString() == "5")
+			{
+				return "Male";
+			}
+			else
+			{
+				if (cnp[0].ToString() == "2" || cnp[0].ToString() == "6")
+				{
+					return "Female";
+				}
+				return "Alien?";
+			}
+		}
+
+		/// <summary>
+		/// Get User's date of birth form CNP
+		/// </summary>
+		/// <param name="cnp"></param>
+		/// <returns></returns>
+		public static DateTime getDateOfBirth(string cnp)
+		{
+			string dateString = cnp[5].ToString() + cnp[6].ToString() + "/" + cnp[3].ToString() + cnp[4].ToString() + "/" + cnp[1].ToString() + cnp[2].ToString();
+			return DateTime.Parse(dateString);
 		}
 	}
 }
